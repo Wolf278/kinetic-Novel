cb4b4c520c0720ca3077301fef009cfc44a80e7d
Wolf278 <86045007+Wolf278@users.noreply.github.com>
2021-08-03 23:44:01 -0500

    Add files via upload

---
+++ Poetry New Beginnings Vol 1.js
@@ -0,0 +1,87 @@
+export const story = {
+"Ch1": {
+    dialog: [
+      #(Thankingyoubeeremastere3.mp3)
+      "Its as if I seen a dragon."
+      "Perfect pitch of darkness."
+      "Are we just as the old."
+      "Or will we create new ways?"
+      @Five("Odd what are you going on about?")
+      @Odd Boy (Left)"Nothing Im sorry brother."
+      @Five(Right) "Its okay brother, we don't have all day."
+      @Odd Boy (Left)"Yes your right."
+    
+      
+      
+      ]
+      "Ch2": {
+        dialog: [
+          #( Atheist Remastered.mp3)
+          @Death (left) "So he's just finished his qourt?"
+          @Nine(right) "Yes the time have final come."
+          @Death "This is all for my desperation?"
+          @Nine "Your love is what made this happen, be great full, thanks to me you still station in a wonderful world of immortality."
+          @Death " Is this the world are son's truelove's."
+          "Man walk in many salicylic              Yet we don't see                 the end is now."
+          
+          
+          
+          
+          
+          
+          ]
+      }
+      
+      "Ch3": {
+        [
+          
+          #(Madmind.mp3)
+          @Five "This is the end of you hahahahah."
+          @Starfier "Man why are you so good at this game?"
+          @Odd Boy (Left) "Okay guys thats enough of chaces, my god youall play to load for a game of patience."
+          @Five (Right) "Hahah!!!"
+          @Starfier (Right) "Yeah yeah, I get you, Im a looser."
+          @Five (Left) "If you keep thinking that way you all was be."
+          @Odd Boy (Right) "Wait I hear my father coming."
+          @New Man (Left) "Son is there something worng?"
+          @Odd Boy "Why you ask?"
+          @New Men "Well, you seem normal???"
+          @Odd Boy "Okay???"
+          @New Man "Just to let you know anything you need just come to me okay son?"
+          @Odd Boy "Okay dad."
+          @Starfier (left) "You know life would be easier if we all hade Odd biological father as a father."
+          @Five (Right)"......"
+          
+          
+          
+          ]
+      }
+      
+     "Ch4": {
+       [
+         #(Thanking you bee remaster 3.mp3)
+         "We live as if we see ghost everyday               But play off as if its not real              Wyh hummu suffering is justified."
+         "I sit as my love play his games wight in his book notes of lyes,            To set me free from new power."
+         @Destonshen "So why are you so sad today?"
+         @Odd Boy (Left) "This isn't right how can my past not be the same mintly but, not phicaly"
+         @Destonshen (Right) "You going in the worng things in life, take Samuel words to hart."
+         @Odd Boy "The leffs and pattels?"
+         @Destonshen "There are green left and a red pattel, there are two different worlds yet, you are a lefel."
+         @Odd Boy "I don't try to be in two wolds."
+         @Destonshen "No, you must find your place in the world you live in behon this."
+         @Odd Boy "But, your word is what you want."
+         @Destonshen "There is only one for you, the one ment for you, please don't put this on one pedestal. Please move on and find you salvation."
+         "Confused on this world                 Crying for a new hope I just can't stand anymore."
+         
+         
+         
+         "Part one Ends"
+         "Thank you for your time."
+         "Porety Evil Good Devil will come soon."
+        
+        
+        
+        
+        
+        ]
+     }
